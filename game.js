ea85bb5807eea44528273222393b21768d65a0ef
p-herbert <pete.a.herbert@gmail.com>
2017-02-23 22:00:59 -0800

    (feat) Create class BattleShip

---
+++ game.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt');
+const Player = require('./player');
+const Board = require('./board');
+
+class BattleShip {
+  constructor() {
+    this.playerOne = null;
+    this.playerTwo = null;
+  }
+}
