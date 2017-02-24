822bda4d288191d2ea48f7c264a75704c9cd5261
p-herbert <pete.a.herbert@gmail.com>
2017-02-24 01:39:33 -0800

    (feat) Create helper convert

--- game.js
+++ game.js
@@ -10,6 +10,30 @@ const shipSize = {
   Submarine: 3,
   Destroyer: 2 };
 
+// Convert User Input
+function convert(input) {
+  let start = input.split(' ')[0];
+  let end = input.split(' ')[1];
+  let coords = null;
+
+  // Swap
+  let temp;
+
+  if (start > end) {
+    temp = start;
+    start = end;
+    end = temp;
+  }
+
+  if (Board.sameRow(start, end)) {
+    coords = Board.rowCoords(start.charAt(1), start.charAt(0), end.charAt(0));
+  } else if (Board.sameCol(start, end)) {
+    coords = Board.colCoords(start.charAt(0), +start.charAt(1), +end.charAt(1));
+  }
+
+  return coords;
+}
+
 class BattleShip {
   constructor() {
     this.playerOne = null;
